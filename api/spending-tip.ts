6edaf42d5474fd0198af78e603c885b26a0c14b9
Dawsan <Dawsan.nm@gmail.com>
2026-02-12 17:37:23 +0700

    vercel 5

--- api/spending-tip.ts
+++
@@ -1,7 +0,0 @@
-type Handler = (req: any, res: any) => any;
-
-export default (async function handler(req: any, res: any) {
-  const mod: any = await import('./spending-tip.js');
-  const fn: Handler = mod?.default || mod;
-  return fn(req, res);
-} satisfies Handler);
