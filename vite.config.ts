a17f9807af4c4cf24156cb09352f1efbe91abe1b
Dawsan <Dawsan.nm@gmail.com>
2026-02-16 14:52:59 +0700

    bismillah final fix

--- vite.config.ts
+++ vite.config.ts
@@ -58,7 +58,7 @@ export default defineConfig(() => {
       },
       build: {
         target: 'esnext',
-        outDir: path.resolve(rootDir, '../dist'),
+        outDir: path.resolve(rootDir, 'dist'),
         emptyOutDir: true,
         sourcemap: true,
       }
